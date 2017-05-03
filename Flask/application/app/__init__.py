f2b7a00c271c5c49c3a0ab066ff08366737f548f
ivanlmj <ivanlmj@gmail.com>
2017-05-03 17:19:12 -0500

    adding view/route "/login" for app module

--- Flask/application/app/__init__.py
+++ Flask/application/app/__init__.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env
 
-from flask import Flask
+from flask import Flask, request
+import mdb_connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def func_root():
     return "<h1> Flask is running! </h1>"
+
+@app.route('/login', methods=['POST'])
+def func_login():
+    username = request.form['username']
+    password = request.form['password']
+    login = mdb_connector.Credentials(username,password)
+    login_data = login.check()
+    return login_data                
+
