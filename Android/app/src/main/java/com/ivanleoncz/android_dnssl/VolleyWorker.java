ce7abaa0b0617429d5c2c865a247eae142e0704a
ivanlmj <ivanlmj@gmail.com>
2017-05-10 23:18:49 -0500

    improving class structure - adding class interface - protoryping HTTP Request (POST)

--- Android/app/src/main/java/com/ivanleoncz/android_dnssl/VolleyWorker.java
+++ Android/app/src/main/java/com/ivanleoncz/android_dnssl/VolleyWorker.java
@@ -1,8 +1,70 @@
 package com.ivanleoncz.android_dnssl;
 
-/**
- * Created by ivanleon on 09/05/17.
- */
+import android.content.Context;
+import android.widget.Toast;
 
-public class VolleyWorker {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class VolleyWorker {
+
+    private String username;
+    private String password;
+
+    private RequestQueue requestQueue;
+
+    VolleyWorker(String usernameStr, String passwordStr) {
+        username = usernameStr;
+        password = passwordStr;
+    }
+
+    interface LoginCallback {
+        void onSuccess(String result);
+    }
+
+    void login(final LoginCallback loginCallback){
+
+        final String url = "http://192.168.100.17:5000/login";
+
+        // beginning of the request --->
+        StringRequest postLogin = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        loginCallback.onSuccess(response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }) { // post parameters
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String> params = new HashMap<>();
+                            params.put("username", username);
+                            params.put("password", password);
+                        return params;
+                    }
+
+        };
+        // <--- enf of the request
+
+        try {
+            requestQueue.add(postLogin);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
