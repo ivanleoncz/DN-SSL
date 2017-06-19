4c342959441f14681b897f8b44ce7ba96c992185
ivanlmj <ivanlmj@gmail.com>
2017-06-18 23:39:01 -0500

    improving LoginScreen.java
    - adding logging statements
    - changing access modifiers for usernameStr and passwordStr variables
    - fixing volleyWorker object
    - adding variables to volleyWorker callback
    - fixing IF condotion for abstract methods "onSuccess"
    - applying JsonParser if condition is favorable
    - applying bundle for passing username to next activity
    - applying animation when calling next activity (overridePendingTransition)

--- Android/app/src/main/java/com/ivanleoncz/android_dnssl/LoginActivity.java
+++ Android/app/src/main/java/com/ivanleoncz/android_dnssl/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.ivanleoncz.android_dnssl;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,33 +22,49 @@ public class LoginActivity extends AppCompatActivity {
 
         username = (EditText) findViewById(R.id.login_username);
         password = (EditText) findViewById(R.id.login_password);
+        password.setTypeface(Typeface.DEFAULT);
         password.setTransformationMethod(new PasswordTransformationMethod());
 
     }
 
     public void login(View view) {
 
-        final String usernameStr;
-        final String passwordStr;
+        Log.d("LoginActivity","processing login");
+
+        String usernameStr;
+        String passwordStr;
         final String toastFailed;
 
         usernameStr = username.getText().toString().trim();
         passwordStr = password.getText().toString().trim();
         toastFailed = "Login Failed";
+        Log.d("LoginActivity","username: " + usernameStr);
+        Log.d("LoginActivity","password: " + passwordStr);
 
-        VolleyWorker volleyWorker = new VolleyWorker(usernameStr, passwordStr);
+        VolleyWorker volleyWorker = new VolleyWorker(this,usernameStr, passwordStr);
 
         volleyWorker.login(new VolleyWorker.LoginCallback() {
 
+            String user;
             Intent intent;
+            Bundle bundle;
 
             @Override
             public void onSuccess(String result) {
-                if (result.equals("nok")) {
+                if (result.equals("not_found")) {
+                    Log.d("LoginActivity","Login NOK...");
                     Toast.makeText(getApplicationContext(),toastFailed,Toast.LENGTH_LONG).show();
                 } else {
+                    Log.d("LoginActivity","Login OK!");
+                    JsonParser jp = new JsonParser(result);
+                    jp.parser();
+                    user = jp.username;
                     intent = new Intent(LoginActivity.this, MessageActivity.class);
+                    bundle = new Bundle();
+                    bundle.putSerializable("username",user);
+                    intent.putExtras(bundle);
                     startActivity(intent);
+                    overridePendingTransition(R.anim.left_right_enter, R.anim.left_right_exit);
                 }
             }
 
