1778f8d7b25487a67215109775ca685ded3d3590
ivanlmj <ivanlmj@gmail.com>
2017-06-20 11:11:04 -0500

    finishing activity construction
    - preparing activity to receive data from intent (bundle)
    - creating hyperlink configuration with html libs

--- Android/app/src/main/java/com/ivanleoncz/android_dnssl/MessageActivity.java
+++ Android/app/src/main/java/com/ivanleoncz/android_dnssl/MessageActivity.java
@@ -1,13 +1,37 @@
 package com.ivanleoncz.android_dnssl;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.style.UnderlineSpan;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
 
 public class MessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String link = "https://stackoverflow.com/questions/2012497/accepting-a-certificate-for-https-on-android/40112598#40112598";
+        String hyperLink = "<a href=" + link + "> Press here for more info! </a>";
+        String username = null;
+        TextView tvLink = null;
+        TextView tvWelcome = null;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        username = getIntent().getStringExtra("username");
+        tvLink = (TextView) findViewById(R.id.message_link);
+        tvWelcome = (TextView) findViewById(R.id.message_welcome);
+
+        tvWelcome.setText(username);
+        tvLink.setMovementMethod(LinkMovementMethod.getInstance());
+        tvLink.setText(Html.fromHtml(hyperLink));
+        tvLink.setLinkTextColor(Color.parseColor("#005ce6"));
+
     }
 }
